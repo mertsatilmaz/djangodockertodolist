231d09b92b568aec91d66983e30758dd855afa9a
Mert <mert.satilmaz@gmail.com>
2019-04-30 12:50:28 +0300

    travistryouts

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -75,11 +75,11 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'todolist',
+        'USER': 'mert',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
